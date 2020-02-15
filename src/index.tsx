e32fd990d2298127d9ce0e2232ec9ac507d594af
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-15 07:58:26 -0300

    Setup miragejs server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MirageServer from './miragejs';
+
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
@@ -8,3 +10,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
+
+const { NODE_ENV: environment = 'development' } = process.env;
+MirageServer.makeServer({ environment });
