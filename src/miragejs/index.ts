f97a298ee739949dd8d5083a1efe67f39fd83cfb
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-15 07:58:06 -0300

    Add consultants resource

--- src/miragejs/index.ts
+++ src/miragejs/index.ts
@@ -1,18 +1,23 @@
 // @ts-ignore
-import { Server } from 'miragejs';
+import { Server, Model } from 'miragejs';
 
 import consultants from './fixtures/consultants.json';
 
-const makeServer = ({ environment = 'development' }) =>
-  new Server({
+function makeServer({ environment = 'development' }) {
+  return new Server({
     environment,
     fixtures: { consultants },
+    models: {
+      consultants: Model,
+    },
     seeds(server: { loadFixtures: (fixtureName: string) => void }) {
       server.loadFixtures('consultants');
     },
     routes() {
       this.namespace = 'api';
+      this.resource('consultants');
     },
   });
+}
 
 export default { makeServer };
