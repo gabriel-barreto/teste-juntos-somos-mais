b59258c40f13e67a6f1cf077282d0416d55d43a9
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-15 18:54:09 -0300

    Add container global component

---
+++ src/styles/container.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  padding: 0 var(--pd-hz-sm);
+  @media (min-width: 768px) {
+    padding: 0 var(--pd-hz-md);
+  }
+  @media (min-width: 1200px) {
+    padding: 0 var(--pd-hz-lg);
+  }
+  @media (min-width: 1400px) {
+    padding: 0 var(--pd-hz-xl);
+  }
+`;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,3 @@
 export { Colors } from './colors.styles';
+export { default as Container } from './container.styles';
 export { default as GlobalStyle } from './global.style';
